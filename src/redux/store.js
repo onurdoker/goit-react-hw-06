4f7695fb498bbbe02c9d33140d7ca09de6b0136e
Onur DÖKER <odoker@msn.com>
2025-10-07 13:25:15 +0300

    Add contacts slice with Redux Toolkit and integrate redux-persist for state persistence

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,23 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+
+import storage from "redux-persist/lib/storage";
+import { contactsReducer } from "./contactsSlice.js";
+
+const persistConfig = {
+  key: "contacts",
+  storage,
+  whitelist: ["items"],
+};
+
+const persistedContactReducer = persistReducer(persistConfig,
+                                               contactsReducer);
 
 export const store = configureStore({
-                                      reducer: {},
+                                      reducer: {
+                                        contacts: persistedContactReducer,
+                                      },
+                                      middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
                                     });
+
+export const persistor = persistStore(store);
