5cacadf2986f3df73a124d91f7d6a09975f7b012
Onur DÖKER <odoker@msn.com>
2025-10-07 13:33:49 +0300

    Add filters slice with Redux Toolkit and integrate it into the store alongside contacts

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 
-import { store } from "./redux/store.js";
+import store from "./redux/store.js";
 
 import "./index.css";
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,8 @@ import { configureStore } from "@reduxjs/toolkit";
 import { persistReducer, persistStore } from "redux-persist";
 
 import storage from "redux-persist/lib/storage";
-import { contactsReducer } from "./contactsSlice.js";
+import contactsReducer from "./contactsSlice.js";
+import filtersReducer from "./filtersSlice.js";
 
 const persistConfig = {
   key: "contacts",
@@ -13,11 +14,14 @@ const persistConfig = {
 const persistedContactReducer = persistReducer(persistConfig,
                                                contactsReducer);
 
-export const store = configureStore({
-                                      reducer: {
-                                        contacts: persistedContactReducer,
-                                      },
-                                      middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
-                                    });
+const store = configureStore({
+                               reducer: {
+                                 contacts: persistedContactReducer,
+                                 filters: filtersReducer,
+                               },
+                               middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
+                             });
 
 export const persistor = persistStore(store);
+
+export default store;
